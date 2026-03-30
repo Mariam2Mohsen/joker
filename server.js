22cb3b2f47eb3a25329da9b8e5984433ecc4c729
WafaaAMA <wafaa.ashraf0918@gmail.com>
2026-03-30 12:36:11 +0200

    edit2 for connection

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ const port = process.env.PORT ;
 app.use(
   cors({
     origin: [process.env.CLIENT_URL ,'https://petrajuniors.org'],
+    origin: 'http://localhost:3000',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: [
